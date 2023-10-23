10cb507bfc051ad125a3ab18aa153f58ce40bc6e
nielstoemen <nielstoemen@gmail.com>
2023-10-23 14:58:59 +0200

    final day commit
    final day commit

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <script src="js/main.js"></script>
 </head>
 <body>
-    <img src="image/logo.png" class="logo">
+    <img src="image/banner1.jpg" height="300px">
     <nav>
         <a href="index.php"><img src="image/huis.jpg" class="huisfoto" width="40px" height="40px"></a>
         <button class="buttonnav"><a href="index.php" class="navigatie">Home</a></button>
@@ -19,36 +19,23 @@
         <button class="buttonnav"><a href="contact.php" class="navigatie">Contact</a></button>
     </nav>
     <main>
-        <img src="image/eten1.jpg" class="foto1">
-        <div class="inleiding">
-            <div class="inleidingtekst">
-                <h4 class="naam1">T vosje</h4>
-                <p>"Smakelijk voor iedereen" is de kreet die al jaren door Eetpaleis t vosje wordt gehanteerd en in ere wordt gehouden! <br> Want smakelijk eten voor elke gast, daar draait het bij ons om. Het vosje is een establisiment in rosmalen waar u heerlijk en snel kunt lunchen en savonds een frietje kan pakken.</p>
-            </div>
-            <div class="burger1">
-                <img src="image/burger1.jpg" class="burgerfoto1">
-            </div>
-            <div class="tweeinleiding">
-                <h4 class="geschiedenis1">Geschiedenis</h4>
-                <p class="geschiedenis2">De basis van T vosje is gezet in 1993 door Ad en Gerda Vos. Hierna hebben zij het overgedragen aan Peter en Dorien Vos. Toen T vosje eenmaal een sucses werd heeft de kleinzoon van Peter en Dorien Vos uitgebreid naar het derde filiaal. Hierna is T vosje uitgegroeid tot de lekkere snackbar die wij nu kennen.</p>
-            </div>
-        </div>
+    <!-- main input -->
+    <h4>Camping de Maasvalei is een camping dichtbij natuurgebied maasvalei. Het natuurgebied ligt op loopafstand.</h4>
     <footer>
         <div class="opening">
             <h3 class="footertekst">Openingstijden</h3>
-            <p class="footertekst">Dinsdag t/m zondag 11:30 – 21:00u</p>
-            <p class="footertekst">Op maandag zijn wij helaas gesloten</p>
+            <p class="footertekst">Maandag t/m zondag 11:30 – 21:00u</p>
         </div>
         <div class="adres">
             <h3 class="footertekst">Contact</h3>
             <h4 class="footertekst">Adres:</h4>
-            <p class="footertekst">Eetpaleis ’t Vosje</p>
+            <p class="footertekst">Camping de Maasvalei</p>
             <p class="footertekst">Mercuriusplein 86</p>
             <p class="footertekst">5258AW Berlicum</p>
             <h4 class="footertekst">Telefoonnummer:</h4>
             <p class="footertekst">073 503 41 14</p>
             <h4 class="footertekst">Email:</h4>
-            <p class="footertekst">info@eetpaleis-tvosje.nl</p>
+            <p class="footertekst">info@de Maasvalei</p>
         </div>
         <div class="socials">
             <a href="https://www.facebook.com/Eetpaleis-t-Vosje-580914481957929/" target="blank">
